bde7a12c401eeef0aa3359c75251e01dcf8c16e0
lekasp <91938983+lekasp@users.noreply.github.com>
2023-03-17 12:44:01 +0100

    updated parser with metadata information

--- main.py
+++ main.py
@@ -37,8 +37,9 @@ def readcsv(filename):
 # Dictionaries
 locations = readcsv('meta/locations_parser.csv')
 locations_extended = [locations[i] + "." for i in range(len(locations))]
-personal_titles = ["Esq."]
+personal_titles = ["Esq.", "C.B.", "C.V. O."]
 address = ["Castle Mills", "Trafalgar", "Trafalgar Square", "Imperial Buildings", "Cambridge Street"]
+names = ["FALCONER EVANS CROWE", "MAURICE GALLAND", "0ALEXANDER RICHARDSON", "THEO. RUSSELL"]
 titles = readcsv('meta/titles.csv')
 
 def parsing_function(text, words, meta_data):
@@ -61,13 +62,108 @@ def parsing_function(text, words, meta_data):
 
 
 def parse_board_member_page(text, words):
-    return {}
+
+    result = {'transcription': text}
+
+    # Check each line against a regex for name
+    match = re.search(r'(^[A-Z]\. ([A-Z]\.)? ?\w{1,100}|^[A-Z]{1,10} [A-Z]{1,10})', text)
+    if match:
+        result["name_found"] = True
+        result["name"] = match.group(0)
+
+    # Check each line against a regex for Title
+    match = re.search(r'(Officers and Members of the Board|Active Members of the Board|Corresponding Members of the Board|Hon. Members of the Board)', text)
+    if match:
+        result["title_found"] = True
+        result["title"] = match.group(0)
+
+    # Check each word against the list locations_parser.csv
+    for word in words:
+        if word in locations:
+            result["location_found"] = True
+            result["location"] = word
+
+    # Check each word against the list personal_titles
+    for word in text:
+        if word in personal_titles:
+            result["personal_title_found"] = True
+            result["personal_title"] = word
+
+    # Check each line against a regex for function
+    match = re.search(r'(Hon\. President|Hon\. Vice-President|President|Hon\. Treasurer|Vice-President|Secretary-General, Head Office|Hon\. Secretary, Lausanne Branch)', text)
+    if match:
+        result["function_found"] = True
+        result["function"] = match.group(0)
+
+    # Check each line against a regex for Profession
+    match = re.search(r'(H\. B\. M\. Minister|Treasurer Provincial Foreign Bank Ltd\.|H\. B\. M\. Consul-General|H\. B\. M\. Consul)', text)
+    if match:
+        result["profession_found"] = True
+        result["profession"] = match.group(0)
+
+    # Check each word against the list names
+    for word in words:
+        if word in names:
+            result["name_found"] = True
+            result["name"] = word
+
+
+    return result
 
 def parse_new_member_page(text, words):
     return {}
 
 def parse_index_page(text, words):
-    return {}
+
+    result = {'transcription': text}
+
+    # Check each word against the list locations_parser.csv
+    for word in words:
+        if word in locations:
+            result["location_found"] = True
+            result["location"] = word
+
+    # Check each word against the list my_locations_extended list
+    for word in words:
+        if word in locations_extended:
+            result["location_extended_found"] = True
+            result["location_extended"] = word
+
+    # Check each line against a regex for Title
+    """match = re.search(
+        r'^(\w{1,20} ?(\w{1,10})) (\(\w*[^.]\))$|^\w{1,20}$|^\w{1,10} \w{1,10}$|^\w{1,10} \w{1,10} \w{1,15}$|^\w{1,10} \(\w{1,10}, .{1,25}\)$',
+        text)
+    if match:
+        result["title_found"] = True
+        result["title"] = match.group(0)
+
+    # Check each line against the list titles
+    for text in words:
+        if text in titles:
+            result["title_found"] = True
+            result["title"] = word"""
+
+    # Check each line against a regex for Goods in brackets
+    match = re.search(r'(\(.{1,150}\),)', text)
+    if match:
+        result["goods_found"] = True
+        result["goods"] = match.group(0)
+
+    # Check each line against a regex for Address
+    match = re.search(r'((([0-9]{1,4})\/?—??—?\/?[0-9]{1,4}, [A-Z].[^,]{1,20},)|S. W. [0-9].)', text)
+    if match:
+        result["address_found"] = True
+        result["address"] = match.group(0)
+
+    # Check each line against a regex for Company
+    match = re.search(
+        r'(^.{1,30} ? (Frères|Brothers|Co\.|& Co\.,|Ltd\.|& Cie.,|S\. A\.,|A\.-G\.,|A\. G\.) ?(S. A.)?|^[A-Z]. [A-Z]. \w{1,10}|^[A-Z][a-z]{1,3}\. \w{1,10}-?\w{1,10}|^\w{1,10} & \w{1,10}|^[A-Z]. \w{1,10}|.{1,100} Co\.)',
+        text)
+    if match:
+        result["company_found"] = True
+        result["company"] = match.group(0)
+
+    return result
 
 def parse_alphabetical_index_page(text, words):
     # print("Structuring line: " + text)
@@ -86,33 +182,27 @@ def parse_alphabetical_index_page(text, words):
             result["location_extended_found"] = True
             result["location_extended"] = word
 
-    # Check each line against a regex for Title
+    # Check each line against a regex for Company with & Co. etc
     match = re.search(
-        r'^(\w{1,20} ?(\w{1,10})) (\(\w*[^.]\))$|^\w{1,20}$|^\w{1,10} \w{1,10}$|^\w{1,10} \w{1,10} \w{1,15}$|^\w{1,10} \(\w{1,10}, .{1,25}\)$',
-        text)
-    if match:
-        result["title_found"] = True
-        result["title"] = match.group(0)
-
-    # Check each line against a regex for Company
-    match = re.search(
-        r'(^.{1,30} ? (Frères|Brothers|Co\.|& Co\.,|Ltd\.|& Cie.,|S\. A\.,|A\.-G\.,|A\. G\.) ?(S. A.)?|^[A-Z]. [A-Z]. \w{1,10}|^[A-Z][a-z]{1,3}\. \w{1,10}-?\w{1,10}|^\w{1,10} & \w{1,10}|^[A-Z]. \w{1,10}|.{1,100} Co\.)',
+        r'(^.{1,30} ? (Frères|Brothers|Co\.|& Co\.,|Ltd\.|& Cie.,|S\. A\.,|A\.-G\.,|A\. G\.) ?(S. A.)?|^[A-Z]. [A-Z]. \w{1,10}|^[A-Z][a-z]{1,3}\. \w{1,10}-?\w{1,10}|^\w{1,10} & \w{1,10}|^[A-Z]. \w{1,10}|.{1,100} Co\.|^.{1,10}\(.{1,10}\)( ?&? ? (Sons)?)|.{1,10}-.{1,10}, [A-Z][a-z]?.|^[A-Z][a-z]{1,10}, [A-Z]\.)',
         text)
     if match:
         result["company_found"] = True
         result["company"] = match.group(0)
 
-    # Check each line against a regex for Goods in brackets
-    match = re.search(r'(\(.{1,150}\),)', text)
+    # Check each line against a regex for Company with () etc
+    match = re.match(
+        r'(^[A-Z][a-z]{1,8},? ?\([A-Z][a-z]{1,8}\)( ?&? ? (Sons)?))',
+        text)
     if match:
-        result["goods_found"] = True
-        result["goods"] = match.group(0)
+        result["company2_found"] = True
+        result["company2"] = match.group(0)
 
-    # Check each line against a regex for Address
-    match = re.search(r'((([0-9]{1,4})\/?—??—?\/?[0-9]{1,4}, [A-Z].[^,]{1,20},)|S. W. [0-9].)', text)
+    # Check each line against a regex for Page Numbers
+    match = re.search(r'(([0-9]{1,2},? ?){1,15}$|^([0-9]{1,2},? ?){1,15})', text)
     if match:
-        result["address_found"] = True
-        result["address"] = match.group(0)
+        result["page_numbers_found"] = True
+        result["page_numbers"] = match.group(0)
 
     return result
 
@@ -184,5 +274,5 @@ def parse_year(data_folder, output_folder):
                 print("File is excluded: " + file)
 
 if __name__ == "__main__":
-    parse_year('data_1923', 'output_1923')
+    parse_year('data_1921', 'output_1921')
 
